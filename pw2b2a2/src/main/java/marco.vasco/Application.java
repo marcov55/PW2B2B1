12273c39ce28447bae65d07c2dbb6d6e1b9d6636
marcov55 <marcovascogit@gmail.com>
2022-06-15 17:55:38 +0000

    pw2b2a2

---
+++ pw2b2a2/src/main/java/marco.vasco/Application.java
@@ -0,0 +1,16 @@
+package pw2b2a2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+ 
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
+
+    
+
